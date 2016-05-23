b0eb79014a852a51ee396f1294e6efa1ae0ad946
Vinay Sharma <imvinaysharma@yahoo.com>
2016-05-23 10:18:18 -0400

    changed message in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render  text: "Have a nice weekend!"
+    render  text: "Weekend is over now!"
   end
   
 end
