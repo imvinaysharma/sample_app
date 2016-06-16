95431a33f6a0f19c091455160577e7feaedc008a
Vinay Sharma <imvinaysharma@yahoo.com>
2016-06-16 15:36:35 -0400

    finish login and lo out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
  
+  
   get 'users/new'
 
   root 'static_pages#home'
@@ -16,6 +17,12 @@ Rails.application.routes.draw do
   get 'contact'  	=> 'static_pages#contact'
   
   get 'signup' 	=> 'users#new'
+  
+  get  'login'  => 'sessions#new'
+  
+  post 'login'  => 'sessions#create'
+  
+  get 'logout' => 'sessions#destroy'
 
   resources :users
 
