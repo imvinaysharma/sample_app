3fb6ad5e4636cb439b53e122941e9908cb2c454a
imvinaysharma <imvinaysharma@yahoo.com>
2016-06-21 14:12:22 -0400

    correct login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
    if user && user.authenticate(params[:session][:password])
        log_in user
 	 #  remember user
-	   params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+	 #  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 	   redirect_back_or user
 	 else
        flash.now[:danger] = "iiiiiiInvalid email/password combination"
