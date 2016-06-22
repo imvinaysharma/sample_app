1b17fb2e84fcfd8926e282c73ecd901184398c7d
imvinaysharma <imvinaysharma@yahoo.com>
2016-06-22 12:08:37 -0400

    change 1 user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Vinay Sharma",
+             email: "vinay@email.com",
+             password:              "password",
+             password_confirmation: "password",
 			 admin: true)
 
 
