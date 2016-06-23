43b9aac139874b03a00a980b064c19c591573bd1
imvinaysharma <imvinaysharma@yahoo.com>
2016-06-23 11:39:20 -0400

    added view port tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@ User.create!(name:  "Vinay Sharma",
 			 admin: true)
 
 
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
